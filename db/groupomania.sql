3efe85fa2a14a5ca0dd746e07951a5f88659a505
Louis <louisfon@orange.fr>
2022-10-25 10:18:54 +0200

    final  sql commit

---
+++ db/groupomania.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `user` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL
+);
+
+CREATE TABLE `post` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `titre` varchar(50) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `user_id` int(int)
+);
+
+CREATE TABLE `comment` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `content` varchar(50),
+  `user_id` int(11),
+  `post_id` int(11)
+);
+
+CREATE TABLE `like` (
+  `user_id` int(11),
+  `post_id` int(11)
+);
+
+ALTER TABLE `post` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `comment` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `like` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `like` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
+
+ALTER TABLE `comment` ADD FOREIGN KEY (`post_id`) REFERENCES `post` (`id`);
